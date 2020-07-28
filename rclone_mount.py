f3097bd431ee17acea29fef91e2a96bb681e3faf
ramanverma-dev <ramanverma4183@gmail.com>
2020-07-28 22:41:50 +0530

    import cross-platform cloud drive mounting script using rclone

---
+++ rclone_mount.py
@@ -0,0 +1,5 @@
+import subprocess
+
+remotes = ["Edu:", "Mavi_New:", "Mavi_Old:", "Mavi_Alvro:"]
+mount_points = ["D:", "F:", "G:", "H:"]
+mount_names = ["EduDrive", "Mavi_New", "Mavi_Old", "Mavi_Alvro"]
