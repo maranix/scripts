64107877a13a6226a4fe1172702608d9b4269afd
ramanverma-dev <ramanverma4183@gmail.com>
2020-07-29 23:24:34 +0530

    rclone_mount: make mounting process completely autonomous * Automatically create and store random mount points * While generating mount points check for any existing drives with similar names   if it exists then allocate another one until there are no remotes left.
    As of this update, i am not sure that the script works for linux right now because i am currently using windows.
    Signed-off-by: ramanverma-dev <ramanverma4183@gmail.com>

--- rclone_mount.py
+++ rclone_mount.py
@@ -1,17 +1,26 @@
-import subprocess
-import platform
+import subprocess, platform, os, random, string
 
 remotes = ["Edu", "Mavi_New", "Mavi_Old", "Mavi_Alvro"]
-mount_points = ["D", "F", "G", "H"]
+mount_points = []
+
+def createMounts():
+    for i in range(len(remotes)):
+        point = random.choice(string.ascii_uppercase)
+        if os.system("vol {}: 2>nul>nul".format(point)) == 0:
+            print("{} mount point already exists, generating another one.".format(point))
+            i=i-1
+        else:
+           mount_points.append(point)
 
 if __name__ == "__main__":
 
     print("Detecting platform....")
     print("Platform type: {}".format(platform.system()))
+    print("Creating mount points")
+    createMounts()
     if platform.system() == "Linux":
-        print("Creating mount points in /tmp/")
         for i in range(len(mount_point)):
-            subprocess.run(["mkdir", "~/CloudDrives/{}".format(mount_point[i])])
+            subprocess.run(["mkdir", "~/CloudDrives/{}".format(mount_points[i])])
 
         print("Inserting drives into their respective mount points")
         for i in range(len(remotes)):
