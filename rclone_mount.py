1ad11d2a4648728d54e0d9a9f20f3d8805c67df3
ramanverma-dev <ramanverma4183@gmail.com>
2020-08-02 23:48:55 +0530

    rclone_mount: Make it prettier and sophisticated
    * Fixed for linux(idk if it works but it should)
    * Make seperate functions for both windows and linux to better distinguish between them.
    * Code refactor in both linux and windows part of the script.
    * Removed useless or unneeded code to improve readability and runtime.
    * There was no need to create 4 mount points for windows as well because as of now only one instance of rclone can be used.
    * Just use the names of remotes to create mount directories in ~/CloudDrive/ on linux because we can xD.
    * Instead of using a loop to check if a volume exists with the same name just call the function again and create a new one rather than going through the code by the number of remotes if it exists. Previous process was useless anyways since we can only mount 1 drive at a time on windows without cluttering up the workspace with cmd windows XD.
    * Process runtime drastically reduced on both platforms.
    
    Signed-off-by: ramanverma-dev <ramanverma4183@gmail.com>

--- rclone_mount.py
+++ rclone_mount.py
@@ -3,42 +3,46 @@ import subprocess, platform, os, random, string
 remotes = ["Edu", "Mavi_New", "Mavi_Old", "Mavi_Alvro"]
 mount_points = []
 
-def createMounts():
+def genChar():
+    point = random.choice(string.ascii_uppercase)
+
+    if os.system("vol {}: 2>nul>nul".format(point)) == 0:
+        genChar()
+    else:
+       mount_points.append(point)
+
+def linux():
     for i in range(len(remotes)):
-        point = random.choice(string.ascii_uppercase)
-        if os.system("vol {}: 2>nul>nul".format(point)) == 0:
-            print("{} mount point already exists, generating another one.".format(point))
-            i=i-1
-        else:
-           mount_points.append(point)
+        subprocess.run(["mkdir", "~/CloudDrives/{}".format(remotes[i])])
+        subprocess.run(["rclone", "mount", "{}:".format(remotes[i]), "~/CloudDrives/{}:".format(remotes[i]), "--allow-non-empty", "--daemon"])
+    
+    print("All drives mounted successfuly")
 
-if __name__ == "__main__":
-
-    print("Detecting platform....")
-    print("Platform type: {}".format(platform.system()))
-    print("Creating mount points")
-    createMounts()
-
-    # Probably broken for now
-    if platform.system() == "Linux":
-        for i in range(len(mount_point)):
-            subprocess.run(["mkdir", "~/CloudDrives/{}".format(mount_points[i])])
-
-        print("Inserting drives into their respective mount points")
-        for i in range(len(remotes)):
-            subprocess.run(["rclone", "mount", "{}:".format(remotes[i]), "~/CloudDrives/{}:".format(mount_points[i]), "--allow-non-empty", "--daemon"])
-        print("All drives mounted successfuly")
-    elif platform.system() == "Windows":
-        print("""
-        Warning working environment is Windows
+def windows():
+    print("""
+        Warning working environment is Windows,
         Currently rclone doesn't run as a background service on windows so only a single
         instance can be executed at once.
 
         Make Sure You Have WinFSP installed to mount drives as network drives.
         """)
-        print("Select a drive to mount:")
-        for i in range(len(remotes)):
-            print("%d.{}".format(remotes[i]) % i)
-        m = int(input())
-        print("Mounting {}".format(remotes[m]))
-        subprocess.run(["powershell", "Start-Job", "-Name " + "\"%s\"" % remotes[m], "-ScriptBlock", "{", "rclone", "mount", "{}:".format(remotes[m]), "{}:".format(mount_points[m]), "--fuse-flag", "--VolumePrefix=\server\{}".format(remotes[m]), "--vfs-cache-mode", "writes", "}", ";", "echo", "\"Mounting Successful\"", ";" , "PAUSE" ])
+
+    for i in range(len(remotes)):
+        print("%d.{}".format(remotes[i]) % i)
+
+    genChar()
+    m = int(input())
+
+    print("Mounting {}".format(remotes[m]))
+
+    subprocess.run(["powershell", "Start-Job", "-Name " + "\"%s\"" % remotes[m], "-ScriptBlock", "{", "rclone", "mount", "{}:".format(remotes[m]), "{}:".format(mount_points[m]), "--fuse-flag", "--VolumePrefix=\server\{}".format(remotes[m]), "--vfs-cache-mode", "writes", "}", ";", "echo", "\"Mounting Successful\"", ";" , "PAUSE" ])
+
+if __name__ == "__main__":
+
+    print("Detecting platform....")
+    print("Platform type: {}".format(platform.system()))
+
+    if platform.system() == "Linux":
+        linux()
+    elif platform.system() == "Windows":
+        windows()
