9cc6da93959a5ce835964cb7fad6beeb4fccb997
ramanverma2k <ramanverma4183@gmail.com>
2022-12-06 12:11:26 +0530

    probably working playbook for fedora

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+OS=$(lsb_release -si)
+
+install() {
+  echo "Installing ansible & Executing playbook"
+
+  case "$OS" in
+    Fedora)
+    set -x
+    sudo dnf update -y
+    sudo dnf install -y ansible
+    ansible-playbook ./playbooks/fedora.yaml --ask-become-pass
+    set +x
+    ;;
+    *)
+    echo "Unsupported OS"
+    ;;
+  esac
+}
