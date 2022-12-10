837c5bb29f3b3da6f0e19f6367a1d3f6b953dc4f
ramanverma2k <ramanverma4183@gmail.com>
2022-12-10 12:40:23 +0530

    setup.sh: only modify dnf.conf if already haven't

--- setup.sh
+++ setup.sh
@@ -10,8 +10,10 @@ install() {
   case "$OS" in
     Fedora)
     set -x
-    sudo sh -c 'echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf'
-    sudo sh -c 'echo "fastestmirror=True" >> /etc/dnf/dnf.conf'
+    if [[ $(grep -rl "max_parallel_downloads" /etc/dnf/dnf.conf) = '' ]]; then
+      sudo sh -c 'echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf'
+      sudo sh -c 'echo "fastestmirror=True" >> /etc/dnf/dnf.conf'
+    fi
     sudo dnf update -y
     sudo dnf install -y ansible
     ansible-playbook ./playbooks/fedora.yaml --ask-become-pass
